6128522bce3c44188aff77fb6b4826202d952f19
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-06-22 17:49:19 -0700

    updated hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,8 +5,8 @@ const Hero = props => (
 <div class="hero">
 <BackgroundVideo />
 <div className="overlay-bottom"></div>
-<h1 className="text-xxl text-white ro">TY</h1>
-<h1 className="text-xxl text-white cio">LER</h1>
+<h1 className="text-xxl text-white ro">Welcome</h1>
+{/* <h1 className="text-xxl text-white cio">LER</h1> */}
 <p className="hero-desc text-white ">Co-Founder of 0ff & 0x I Digital Marketing I E-Commerce I Shopify Web Design</p>
 </div>
 );
